9a7ce9646cfe93f8a616c7e82b78269e646f2057
Brokkoly <brendankrull@gmail.com>
2020-09-23 14:51:00 -0500

    Fill out the help section.

--- ClientApp/src/components/About.tsx
+++ ClientApp/src/components/About.tsx
@@ -7,21 +7,21 @@ export const AboutSection: React.FunctionComponent = () =>
         <div className="textColor _aboutSection">
             <h3>About The Bot</h3>
 
-            <p>
+            <p className="_textIndent">
                 Brokkoly Bot is a Discord bot made to make life slightly easier on your server. At its core it is a quote bot made for retrieval of random hot takes or memorable quotes from the server's past. Instead of finding a link that people ask for on a daily basis, just use !command and the bot will send the preset response with the link.
             </p>
-            <p>
+            <p className="_textIndent">
                 The bot is built in Python with a PostgreSQL backend. The hosting and automatic deployment is done on Heroku. Check out the bot's code <a href="https://github.com/Brokkoly/BrokkolyBot">here.</a>
             </p>
             <h3>About This Website</h3>
-            <p>
+            <p className="_textIndent">
                 This website came along because I needed a good way for users to see what commands the bot has for their server, and having users edit settings through a text interface is not a good experience.
             </p>
-            <p>
+            <p className="_textIndent">
                 This website's client is built using Typescript and React. The web server is built using Asp.Net Core, which provides the apis used by the client in addition to connecting to the same PostgreSQL database used by the bot. The website is hosted on Azure. Check out the website's code <a href="https://github.com/Brokkoly/BrokkolyBotFrontend">here.</a>
             </p>
             <h3>Other Information</h3>
-            <p>
+            <p className="_textIndent">
                 Having trouble with the bot or this website? Leave an issue on the appropriate github repository, or send me an email <a href="mailto:brokkolybot@gmail.com">here.</a>
             </p>
 
--- ClientApp/src/components/Help.tsx
+++ ClientApp/src/components/Help.tsx
@@ -6,28 +6,29 @@ export const HelpSection: React.FunctionComponent = () =>
     return (
         <div className="textColor _aboutSection">
             <h3>Commands</h3>
-            <p>
-                Commands are the bread and butter of BrokkolyBot. Simply type !commandName and the bot will respond with a random message you've associated with commandName. Head over to "My Servers" to check out the existing commands on your servers.
+            <p className="_textIndent">
+                Commands are the bread and butter of BrokkolyBot. Simply type !commandName and the bot will respond with a random message you've associated with commandName. Head over to "My Servers" to check out the existing commands on your servers. If a command has multiple messages associated with it, !commandName will retrieve a random message from the group.
             </p>
             <h3>!Help</h3>
-            <p>
+            <p className="_textIndent">
                 !help will get you a dm from the bot, letting you know all of the commands as well as all of the commands the server has available.
             </p>
-            <h3>Extracting Emojis</h3>
-            <p>
-                !extractemoji will get the url to emojis later in your message. You can also pass a link to a discord message and the bot will extract the emojis in that message. Finally you can get the images for emojis when on mobile.
+            <h3>!ExtractEmoji</h3>
+            <p className="_textIndent">
+                !extractemoji will get the url to emojis in your message. You can also pass a link to a discord message and the bot will extract the emojis in that message. Finally you can get the images for emojis when on mobile.
             </p>
             <h3>Cooldowns</h3>
-            <p>
+            <p className="_textIndent">
                 Managers can set a cooldown for the server. The bot can only respond to messages once per cooldown period.
             </p>
             <h3>Managers and Manager-Only Commands</h3>
-            <p>
+            <p className="_textIndent">
                 If you have the Manage Guild permission on discord, or you have the bot's manager role on your server, you can add commands to the bot on the server or on this website. Managers can also use the manager-only commands listed below:
             </p>
             <h3>!Add</h3>
-            <p>
-                !Add &ltcommandName&gt &ltMessage&gt will add a new response for commandName. If a command has multiple messages associated with it, !commandName will retrieve a random message from the group.
+            <p className="_textIndent">
+                !Add &lt;commandName&gt; &lt;Message&gt; will add a new response for commandName.
+                <br/>
                 Restrictions:
                 <ul>
                     <li>The commandName must be longer than 3 characters and cannot be longer than 20 characters. It can only contain letters.</li>
@@ -38,11 +39,11 @@ export const HelpSection: React.FunctionComponent = () =>
                 </ul>
             </p>
             <h3>!Timeout</h3>
-            <p>
-                !Timeout @User &ltimeInSeconds&gt will assign a role that prevents the user from sending messages in any channels for the provided time. After the time has elapsed, the bot will automatically remove the role. The bot will create a role for this purpose, using its nickname on the server (eg: "Brokkoly Bot's Timeout Role"). This role's name can be modified freely.
+            <p className="_textIndent">
+                !Timeout @User &lt;timeInSeconds&gt; will assign a role that prevents the user from sending messages in any channels for the provided time. After the time has elapsed, the bot will automatically remove the role. The bot will create a role for this purpose, using its nickname on the server (eg: "BrokkolyBot's Timeout Role"). This role's name can be modified freely.
             </p>
             <h3>!RemoveTimeout</h3>
-            <p>
+            <p className="_textIndent">
                 !RemoveTimeout @User will remove the timeout role from a user before their sentence is up.
             </p>
         </div>
