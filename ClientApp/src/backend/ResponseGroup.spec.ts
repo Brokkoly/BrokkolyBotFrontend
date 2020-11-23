1e502a9c24aa3a57c3c1358f33d4d239ec9b66e8
Brokkoly <brendankrull@gmail.com>
2020-11-23 13:11:29 -0600

    Moved ResponseGroup stuff to a new file

---
+++ ClientApp/src/backend/ResponseGroup.spec.ts
@@ -0,0 +1,162 @@
+﻿import { Response, ResponseGroup, ResponseGroupList } from "./ResponseGroup";
+
+const reallyLongCommand = '0123456789012345678901234567890123456789012345678901234567890123456789012345678901234567890123456789' +
+    '0123456789012345678901234567890123456789012345678901234567890123456789012345678901234567890123456789' +
+    '0123456789012345678901234567890123456789012345678901234567890123456789012345678901234567890123456789' +
+    '0123456789012345678901234567890123456789012345678901234567890123456789012345678901234567890123456789' +
+    '0123456789012345678901234567890123456789012345678901234567890123456789012345678901234567890123456789' +
+    '0123456789012345678901234567890123456789012345678901234567890123456789012345678901234567890123456789' +
+    '0123456789012345678901234567890123456789012345678901234567890123456789012345678901234567890123456789' +
+    '0123456789012345678901234567890123456789012345678901234567890123456789012345678901234567890123456789' +
+    '0123456789012345678901234567890123456789012345678901234567890123456789012345678901234567890123456789' +
+    '0123456789012345678901234567890123456789012345678901234567890123456789012345678901234567890123456789';
+
+const responseGroupList = new ResponseGroupList(
+    {
+        responseGroups: [
+            new ResponseGroup(0, "notmodonly",
+                [
+                    new Response({ id: 294, response: "No need to be a mod.", modOnly: 0 })
+                ]),
+            new ResponseGroup(1, "bigbrain",
+                [
+                    new Response({ id: 271, response: "edited this", modOnly: 0 })
+                ]),
+            new ResponseGroup(2, "todo",
+                [
+                    new Response({ id: 296, response: "Figure out the mods only hat", modOnly: 0 }),
+                    new Response({ id: 297, response: "Stuff doesn't grey out on accept anymore.", modOnly: 0 })
+                ]),
+            new ResponseGroup(3, "testtwo",
+                [
+                    new Response({ id: 272, response: "asdf", modOnly: 0 })
+                ]),
+            new ResponseGroup(4, "test",
+                [
+                    new Response({ id: 247, response: "New Value New Value", modOnly: 0 }),
+                    new Response({ id: 279, response: "blah", modOnly: 0 }),
+                    new Response({ id: 274, response: "Asdf", modOnly: 0 }),
+                    new Response({ id: 266, response: "This is a test command", modOnly: 0 }),
+                    new Response({ id: 278, response: "asdf", modOnly: 0 })
+                ]),
+            new ResponseGroup(5, "lorem",
+                [
+                    new Response({
+                        id: 267,
+                        response: "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.",
+                        modOnly: 0
+                    })
+                ]),
+            new ResponseGroup(6, "purpose",
+                [
+                    new Response({ id: 280, response: "What is my purpose", modOnly: 0 })
+                ]),
+            new ResponseGroup(7, "yourepeatshitposts",
+                [
+                    new Response({ id: 281, response: "Oh God", modOnly: 0 })
+                ]),
+            new ResponseGroup(8, "modsonly",
+                [
+                    new Response({ id: 293, response: "haha i'm a mod", modOnly: 1 })
+                ]),
+            new ResponseGroup(9, "testneedsnewresponse", []),
+            new ResponseGroup(10, "testneedsnewresponsetwo",
+                [
+                    new Response({ id: 2074, response: "Asdf", modOnly: 0 }),
+                    new Response({ id: 2066, response: "This is a test command", modOnly: 0 }),
+                    new Response({ id: -2, response: "", modOnly: 0 }),
+                ]),
+            new ResponseGroup(11, "testneedsnewresponsetwo",
+                [
+                    new Response({ id: 20741, response: "Asdf", modOnly: 0 }),
+                    new Response({ id: -3, response: "", modOnly: 0 }),
+                    new Response({ id: 20661, response: "This is a test command", modOnly: 0 }),
+                ]),
+            new ResponseGroup(12, "testneedsnewresponsethree",
+                [
+                    new Response({ id: 20742, response: "Asdf", modOnly: 0 }),
+                    new Response({ id: 20662, response: "This is a test command", modOnly: 0 }),
+                    new Response({ id: -4, response: "", modOnly: 0 }),
+                ])
+
+        ]
+    }
+);
+
+describe('Find Response Index Tests', () =>
+{
+
+    const testCasesResponses = [
+        {
+            groupId: 1,
+            id: 271,
+            expectedIndex: 0
+        },
+        {
+            groupId: 34,
+            id: 271,
+            expectedIndex: -1
+        },
+        {
+            groupId: 5,
+            id: 267,
+            expectedIndex: 0
+        },
+        {
+            groupId: 4,
+            id: 279,
+            expectedIndex: 1
+        },
+        {
+            groupId: 4,
+            id: 1,
+            expectedIndex: -1
+        },
+    ];
+
+    testCasesResponses.forEach(test =>
+    {
+        it(`Should find the index of the response with id ${test.id} in the group with id ${test.groupId}. Expected ${test.expectedIndex}`, () =>
+        {
+            let rgi: number = responseGroupList.findResponseGroupIndex(test.groupId);
+            const responseIndexInGroup = responseGroupList.responseGroups[rgi]?.findResponse({ id: test.id }).index ?? -1;
+            expect(responseIndexInGroup).toEqual(test.expectedIndex);
+
+        });
+    });
+})
+
+describe('Find Response Group Index Tests', () =>
+{
+    const testCasesList = [
+        {
+            id: 1,
+            expectedIndex: 1
+        },
+        {
+            id: 34,
+            expectedIndex: -1
+        },
+        {
+            id: 4,
+            expectedIndex: 4
+        },
+        {
+            id: 5,
+            expectedIndex: 5
+        },
+        {
+            id: 0,
+            expectedIndex: 0
+        },
+    ];
+
+    testCasesList.forEach(test =>
+    {
+        it(`Should find the index of the group whose original command is ${test.id}`, () =>
+        {
+            const groupIndex = responseGroupList.findResponseGroupIndex(test.id);
+            expect(groupIndex).toEqual(test.expectedIndex);
+        })
+    })
+})
---
+++ ClientApp/src/backend/ResponseGroup.ts
@@ -0,0 +1,438 @@
+﻿import { Commands } from "./Commands";
+import { CommandValidationError, ErrorLevels, Errors, ValueValidationError } from "./Error";
+
+
+export interface IUpdateResponseActionProps
+{
+    id: number;
+    newResponse?: string;
+    newModOnlyValue?: number;
+    deleted?: boolean;
+}
+export interface IUpdateResponseProps extends IUpdateResponseActionProps
+{
+    groupId: number;
+}
+
+
+export interface IResponseConstructorArgs
+{
+    id: number;
+    modOnly?: number;
+    response: string;
+}
+
+export interface IResponse extends IResponseConstructorArgs
+{
+    id: number;
+    modOnly: number;
+    response: string;
+    deleted: boolean;
+    edited: boolean;
+    errors: Errors;
+    copy(): IResponse;
+    update(args: IUpdateResponseProps): void
+    validate(otherResponses: IResponse[]): Errors;
+}
+
+export class Response implements IResponse
+{
+    id: number;
+    modOnly = 0;
+    response = "";
+    errors: Errors;
+    deleted: boolean;
+    edited: boolean;
+    constructor(args: IResponseConstructorArgs)
+    {
+        this.id = args.id;
+        this.response = args.response;
+        this.modOnly = args.modOnly || 0;
+        this.errors = new Errors();
+        this.deleted = false;
+        this.edited = false;
+    }
+
+    validate(allResponses: IResponse[]): Errors
+    {
+        this.errors = Commands.checkResponseValidity(this.response);
+
+        if (allResponses.find(resp => resp.id !== this.id && resp.response === this.response)) {
+            this.errors.addErrors(new ValueValidationError("Duplicate responses are not allowed"));
+        }
+        return this.errors;
+    }
+
+
+
+    update(args: IUpdateResponseProps)
+    {
+        if (args.newResponse !== undefined) {
+            this.response = args.newResponse;
+        }
+        if (args.newModOnlyValue !== undefined) {
+            this.modOnly = args.newModOnlyValue;
+        }
+    }
+
+    copy(): IResponse
+    {
+        let retResponse = new Response({ id: this.id, response: this.response, modOnly: this.modOnly });
+        retResponse.errors = this.errors;
+        retResponse.deleted = this.deleted;
+        retResponse.edited = this.edited;
+        return retResponse;
+    }
+}
+
+export interface IFindResponseIndexArgs
+{
+    id: number;
+    groupId: number;
+}
+
+export interface IUpdateResponseGroupProps
+{
+    id: number;
+    newCommand?: string;
+    deleted?: boolean;
+    newEditModeStatus?: boolean;
+    newExpandStatus?: boolean;
+    revert?: boolean;
+}
+
+
+export interface IResponseGroupListProps
+{
+    responseGroups: IResponseGroup[];
+}
+
+export interface IResponseGroupList extends IResponseGroupListProps
+{
+    findResponseIndex(args: IFindResponseIndexArgs): number;
+    findResponseGroupIndex(id: number): number
+    findResponseGroup(id: number): IResponseGroup | undefined;
+    copy(): IResponseGroupList;
+    handleResponseUpdate(args: IUpdateResponseProps, tempId: number): void
+    handleResponseGroupUpdate(args: IUpdateResponseGroupProps, tempId: number, userCanManage: boolean): void;
+    validate(idToValidate?: number): void;
+    expandAllGroups(expanded: boolean): void;
+}
+
+/**
+ * Wrapper for a list of response groups. Implements functions that were previously not part of the class definition
+ * */
+export class ResponseGroupList implements IResponseGroupList
+{
+    responseGroups: IResponseGroup[];
+
+    constructor(oldRGL: IResponseGroupListProps)
+    {
+        this.responseGroups = [...oldRGL.responseGroups];
+    }
+
+
+    expandAllGroups(expanded: boolean): void
+    {
+        this.responseGroups.forEach(grp =>
+        {
+            let newRG = grp.copy();
+            newRG.setExpanded(expanded);
+        });
+    }
+
+
+    validate(idToValidate?: number): void
+    {
+        if (idToValidate === undefined) {
+            //Do validation on everything?
+        }
+        else {
+            let groupToValidate = this.responseGroups[this.findResponseGroupIndex(idToValidate)];
+            this.responseGroups.forEach(grp =>
+            {
+                if (grp.id === idToValidate) {
+                    return;
+                }
+                else {
+                    if (groupToValidate.command === grp.command) {
+                        groupToValidate.commandErrors.addErrors(new CommandValidationError("This command already exists", ErrorLevels.Critical));
+                    }
+                }
+            })
+        }
+    }
+
+    findResponseIndex(args: IFindResponseIndexArgs): number
+    {
+        //TODO: call a method on the individual group.
+        let groupIndex = this.findResponseGroupIndex(args.groupId);
+
+        if (groupIndex === -1) {
+            return -1;
+        }
+        return this.responseGroups[groupIndex].findResponse({ id: args.id }).index;
+    }
+
+    findResponseGroupIndex(id: number): number
+    {
+        return this.responseGroups.findIndex(grp => grp.id === id);
+    }
+
+    findResponseGroup(id: number): IResponseGroup | undefined
+    {
+        return this.responseGroups.find(grp => grp.id === id);
+    }
+
+    copy(): IResponseGroupList
+    {
+        return new ResponseGroupList(this);
+    }
+
+    handleResponseUpdate(args: IUpdateResponseProps, tempId: number): void
+    {
+        let groupIndex = this.findResponseGroupIndex(args.groupId);
+        let responseIndex = this.responseGroups[groupIndex].findResponse({ id: args.id }).index;
+        let len = this.responseGroups[groupIndex].responses.length;
+        this.responseGroups = [...this.responseGroups];
+        let groupToUpdate = this.responseGroups[groupIndex];
+        let responseToUpdate = groupToUpdate.responses[responseIndex];
+
+        if (args.newModOnlyValue !== undefined) {
+            responseToUpdate.modOnly = args.newModOnlyValue;
+            responseToUpdate.edited = true;
+        }
+        if (args.newResponse !== undefined) {
+            responseToUpdate.response = args.newResponse;
+            responseToUpdate.edited = true;
+            responseToUpdate.validate(groupToUpdate.responses);
+        }
+        if (args.deleted !== undefined) {
+            responseToUpdate.deleted = args.deleted;
+        }
+        if (responseIndex === len - 1 && groupToUpdate.responses[responseIndex].response !== "") {
+            groupToUpdate.insertNewResponseAtEnd(tempId);
+        }
+    }
+
+    handleResponseGroupUpdate(args: IUpdateResponseGroupProps, tempId: number, userCanManage: boolean): void
+    {
+        this.responseGroups = [...this.responseGroups];
+        let groupToUpdate = this.responseGroups.find(grp => grp.id === args.id);
+        if (!groupToUpdate) {
+            return;
+        }
+        if (args.newExpandStatus !== undefined) {
+            groupToUpdate.setExpanded(args.newExpandStatus);
+        }
+        if (userCanManage) {
+
+            if (args.newCommand !== undefined) {
+                groupToUpdate.updateCommand(args.newCommand);
+                this.validate(groupToUpdate.id);
+            }
+            if (args.newEditModeStatus !== undefined) {
+                groupToUpdate.setEditMode(args.newEditModeStatus);
+            }
+            if (groupToUpdate.inEditMode && groupToUpdate.expanded && groupToUpdate.needsEmptyEntry()) {
+                groupToUpdate.insertNewResponseAtEnd(tempId);
+            }
+            if (this.responseGroups[this.responseGroups.length - 1].command !== "") {
+                let newRespGroup = new ResponseGroup(tempId - 1, "", [], true);
+                newRespGroup.insertNewResponseAtEnd(tempId - 2);
+                this.responseGroups.push(newRespGroup);
+            }
+        }
+    }
+
+
+}
+
+export interface IResponseGroup
+{
+    id: number;
+    command: string;
+    originalCommand: string;
+    responses: IResponse[];
+    expanded: boolean;
+    deleted: boolean;
+    inEditMode: boolean;
+    commandErrors: Errors;
+    groupErrors: Errors[];
+
+
+    copy(): IResponseGroup;
+    findResponse(args: IFindResponseArgs): IResponseReturn;
+    needsEmptyEntry(): boolean;
+    insertNewResponseAtEnd(tempId: number): void;
+    setExpanded(expanded: boolean): void;
+    setEditMode(editMode: boolean): void;
+    getCommandValidity(): Errors;
+    checkCommandValidity(): Errors;
+    getGroupValidity(): Errors[];
+    checkGroupValidity(): Errors[];
+    updateCommand(newCommand: string): void;
+    checkHighestErrorLevel(): number;
+}
+
+export interface IResponseReturn
+{
+    response?: IResponse;
+    index: number;
+}
+
+export interface IFindResponseArgs
+{
+    response?: string;
+    id?: number;
+}
+
+export class ResponseGroup implements IResponseGroup
+{
+    id: number;
+    expanded: boolean;
+    inEditMode: boolean;
+    originalCommand = ""
+    responses: IResponse[] = [];
+    command = "";
+    deleted = false;
+    commandErrors: Errors;
+    groupErrors: Errors[];
+
+    constructor(id: number, command: string, responses: IResponse[], newEmptyGroup?: boolean)
+    {
+        this.id = id;
+        this.command = command;
+        this.originalCommand = command;
+        this.responses = responses || [];
+        this.commandErrors = new Errors();
+        this.groupErrors = this.checkGroupValidity();
+        if (newEmptyGroup) {
+            this.inEditMode = true;
+            this.expanded = true;
+        }
+        else {
+
+            this.expanded = false;
+            this.inEditMode = false;
+        }
+
+    }
+
+    findResponse(args: IFindResponseArgs): IResponseReturn
+    {
+        let index = -1;
+        let retResponse: IResponse | undefined = undefined;
+        if (args.id !== undefined) {
+            index = this.responses.findIndex(response => response.id === args.id);
+        }
+        if (args.response !== undefined) {
+            index = this.responses.findIndex(response => response.id === args.id);
+        }
+        if (index !== -1) {
+            retResponse = this.responses[index];
+        }
+        return { index: index, response: retResponse };
+    }
+
+    getCommandValidity(): Errors
+    {
+        return this.commandErrors;
+    }
+
+    checkCommandValidity(): Errors
+    {
+        this.commandErrors = Commands.checkCommandValidity(this.command);
+        return this.commandErrors;
+    }
+
+    getGroupValidity(): Errors[]
+    {
+        return this.groupErrors;
+    }
+
+    checkGroupValidity(markLastEmptyTempInvalid: boolean = false): Errors[]
+    {
+        if (this.responses === undefined) {
+            return [];
+        }
+        let retErrors: Errors[] = new Array(this.responses.length);
+        this.responses.forEach((resp, index) =>
+        {
+            if (!markLastEmptyTempInvalid && index === this.responses.length - 1 && resp.id < 0 && resp.response === "") {
+                return;
+            }
+            retErrors[index] = (resp as Response).validate(this.responses);
+        })
+        this.groupErrors = retErrors;
+        return retErrors;
+    }
+
+    checkHighestErrorLevel(): number
+    {
+        let highestErrorLevel = 0;
+        this.checkGroupValidity(false).forEach(errors =>
+        {
+            let level = errors.getHighestErrorLevel();
+            if (level > highestErrorLevel) {
+                highestErrorLevel = level;
+            }
+
+        });
+        let level = this.checkCommandValidity().getHighestErrorLevel();
+        if (level > highestErrorLevel) {
+            highestErrorLevel = level;
+        }
+        return highestErrorLevel;
+    }
+
+    /**
+     * Checks if the last response is empty or if it is does not have a temporary id
+     */
+    needsEmptyEntry(): boolean
+    {
+        return this.responses.length === 0 || (this.responses[this.responses.length - 1].response !== "") || (this.responses[this.responses.length - 1].id >= 0);
+    }
+
+    setExpanded(expanded: boolean)
+    {
+        this.expanded = expanded;
+    }
+
+    setEditMode(editMode: boolean)
+    {
+        this.inEditMode = editMode;
+    }
+
+    /**
+     * Adds a new command to the end of the responses array
+     * @param tempId the temporary id to assign to the response
+     */
+    insertNewResponseAtEnd(tempId: number): void
+    {
+        this.responses.push(new Response({ id: tempId, response: "", modOnly: 0 }));
+    }
+
+    updateCommand(newCommand: string): void
+    {
+        this.command = newCommand;
+        this.checkCommandValidity();
+    }
+
+    copy(): IResponseGroup
+    {
+        let retGrp = new ResponseGroup(this.id, this.command, []);
+        retGrp.expanded = this.expanded;
+        retGrp.deleted = this.deleted;
+        retGrp.originalCommand = this.originalCommand
+        retGrp.commandErrors = this.commandErrors;
+        retGrp.groupErrors = this.groupErrors;
+        retGrp.inEditMode = this.inEditMode;
+        this.responses.forEach(rsp =>
+        {
+            retGrp.responses.push(rsp.copy());
+        });
+
+        return retGrp;
+    }
+}
